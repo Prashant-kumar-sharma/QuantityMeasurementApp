55a42ca07e2b22720880433a76cf296e3ce6e07b
Prashant Kumar Sharma <prashant10112003@gmail.com>
2026-04-25 02:43:51 +0530

    Update configurations for production environment

--- auth-service/src/main/java/com/app/quantitymeasurement/config/SecurityConfig.java
+++ auth-service/src/main/java/com/app/quantitymeasurement/config/SecurityConfig.java
@@ -129,7 +129,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://prashantlab.me", "http://13.60.215.51"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"));
         configuration.setAllowCredentials(true);
